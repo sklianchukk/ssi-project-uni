25bf22d5394634d1e1b5849f395f12f9fb3cf513
Bohdan Kostyk <bk318913@student.polsl.pl>
2026-05-01 12:38:56 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import KunstlicheIntel as ki
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sleep = pd.read_csv("sleep_quality.csv")
+print(sleep.describe())
+
+
+sleep[["Systolic Pressure", "Diastolic Pressure"]] = sleep["Blood Pressure"].str.split("/", expand=True).astype(int)
+sleep["Sleep Disorder"] = sleep["Sleep Disorder"].fillna("No Disorder")
+sleep = sleep.drop(columns=["Blood Pressure"])
+print(sleep.isna().sum())
+
+print(sleep.head(10))
+
+sns.pairplot(sleep[["Age", "Sleep Duration", "Quality of Sleep", "Physical Activity Level", "Stress Level", "Heart Rate","Daily Steps", "Systolic Pressure", "Diastolic Pressure","Sleep Disorder"]],hue="Sleep Disorder")
+plt.show()
