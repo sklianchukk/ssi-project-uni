9c64fb38235ada8410b3e6d04204d9fc0fcd24e0
Bohdan Kostyk <bk318913@student.polsl.pl>
2026-05-03 00:33:20 +0200

    Unnecessary imports were removed

--- main_bayes.py
+++ main_bayes.py
@@ -1,11 +1,4 @@
 import pandas as pd
-import numpy as np
-import KunstlicheIntel as ki
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.model_selection import cross_validate, ShuffleSplit
 
 sleep = pd.read_csv("sleep_quality.csv")
 classes = "Sleep Disorder"
